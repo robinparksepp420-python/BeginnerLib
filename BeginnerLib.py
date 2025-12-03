99a3276579b0423d740fde4ae7672ee06f7c149c
robinparksepp420-python <robin.parksepp420@gmail.com>
2025-12-03 14:32:54 +0200

    Add files via upload

---
+++ BeginnerLib.py
@@ -0,0 +1,38 @@
+# Core utilities
+import sys
+import os
+import pathlib
+import time
+import datetime
+import random
+import math
+import functools
+import itertools
+
+# File and data handling
+import csv
+import json
+import pickle
+import shutil
+import glob
+
+# Networking / web
+import socket
+import urllib
+import http
+import ssl
+import requests          # External: easy HTTP requests
+
+# Text / parsing
+import re
+import string
+import html
+
+# Debugging / logging
+import logging
+import traceback
+import warnings
+
+# GUI / graphics
+import tkinter
+import turtle
